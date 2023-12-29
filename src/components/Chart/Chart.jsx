a1a3caea878892e178e0b6aaf0b00b539d70750d
elenitach <lena.01460@gmail.com>
2023-12-29 21:06:47 +0500

    change colors of diagram bars and calendar active day

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,41 +1,57 @@
-import { AgChartsReact } from 'ag-charts-react';
+import { AgChartsReact } from "ag-charts-react";
 
 const Chart = ({ data }) => {
   const options = {
+    theme: {
+      palette: {
+        fills: ["#869aa6", "#f2f1eb"],
+        strokes: ["#283c57"],
+      },
+      overrides: {
+        bar: {
+          series: {
+            highlightStyle: {
+              item: {
+                fill: "rgba(0, 0, 0, 0.2)",
+              },
+            },
+          },
+        },
+      },
+    },
     data,
     series: [
       {
-        type: 'bar',
-        xKey: 'macronutrientType',
-        yKey: 'consumed',
+        type: "bar",
+        xKey: "macronutrientType",
+        yKey: "consumed",
         normalizedTo: 100,
         stacked: true,
       },
       {
-        type: 'bar',
-        xKey: 'macronutrientType',
-        yKey: 'left',
+        type: "bar",
+        xKey: "macronutrientType",
+        yKey: "left",
         normalizedTo: 100,
         stacked: true,
       },
     ],
     axes: [
       {
-        type: 'number',
-        position: 'bottom',
+        type: "number",
+        position: "bottom",
         label: {
-          formatter: (params) => Math.round(params.value) + '%',
+          formatter: (params) => Math.round(params.value) + "%",
         },
       },
       {
-        type: 'category',
-        position: 'left',
+        type: "category",
+        position: "left",
       },
-    ]
+    ],
   };
 
   return <AgChartsReact options={options} />;
-
-}
+};
 
 export default Chart;
